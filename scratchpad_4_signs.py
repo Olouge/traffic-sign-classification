1696d625170f7baac5d7f5e37d866fb1a9e42250
Matthew Zimmer <M16r01@gmail.com>
2016-11-22 22:54:25 -0500

    Just achieved 60% accuracy

--- scratchpad_4_signs.py
+++ scratchpad_4_signs.py
@@ -16,6 +16,14 @@ import numpy as np
 import tensorflow as tf
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+import cv2
+
+
+
+
+
+
+# 1. Load in train and test pickle files
 
 training_file = 'traffic-sign-data/train.p'
 testing_file = 'traffic-sign-data/test.p'
@@ -25,20 +33,18 @@ with open(training_file, mode='rb') as f:
 with open(testing_file, mode='rb') as f:
     test = pickle.load(f)
 
-train_features, train_labels, train_size, train_coords = train['features'], train['labels'], train['sizes'], train['coords']
-test_features, test_labels, test_size, test_coords = test['features'], test['labels'], test['sizes'], test['coords']
+X_train, train_labels, train_size, train_coords = train['features'], train['labels'], train['sizes'], train['coords']
+X_test, test_labels, test_size, test_coords = test['features'], test['labels'], test['sizes'], test['coords']
 
-assert len(train_features) == len(train_labels), 'features must be same size as labels'
+assert len(X_train) == len(train_labels), 'features must be same size as labels'
 
-# Reshape train features
-train_features = np.arange(len(train_features)*1024).reshape((len(train_features), 1024))
-test_features = np.arange(len(test_features)*1024).reshape((len(test_features), 1024))
 
-assert len(train_features) == len(train_labels), 'features must be same size as labels'
 
-_epochs_completed = 0
-_index_in_epoch = 0
-_num_examples = len(train_features)
+# Flatten train and test features
+# X_train = np.arange(len(X_train) * 1024).reshape((len(X_train), 1024))
+# X_test = np.arange(len(X_test) * 1024).reshape((len(X_test), 1024))
+
+# assert len(X_train) == len(train_labels), 'features must be same size as labels'
 
 """
 Helper-function for flattening a layer
@@ -72,6 +78,24 @@ def flatten_layer(layer):
     # Return both the flattened layer and the number of features.
     return layer_flat, num_features
 
+
+def rbg_to_gray(images):
+    # fs for features
+    gray_images = []
+    for i in range(0, images.shape[0]):
+        image = images[i]
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gray_images.append(gray_image)
+    return np.array(gray_images)
+
+def flatten_images(images):
+    flattened_images = []
+    for i in range(0, images.shape[0]):
+        image = images[i]
+        f = np.array(image, dtype=np.float32).flatten()
+        flattened_images.append(f)
+    return np.array(flattened_images)
+
 # Problem 1 - Implement Min-Max scaling for greyscale image data
 def normalize_greyscale(image_data):
     """
@@ -93,9 +117,24 @@ def normalize_greyscale(image_data):
 
 # [Adapted from Lesson 7 - MiniFlow]
 # Turn labels into numbers and apply One-Hot Encoding
-num_channels = 3
-train_features = normalize_greyscale(train_features)
-test_features = normalize_greyscale(test_features)
+print (X_train.shape)
+X_train = rbg_to_gray(X_train)
+X_test = rbg_to_gray(X_test)
+print (X_train.shape)
+
+# Flatten train and test features
+# X_train = np.arange(len(X_train) * 1024).reshape((len(X_train), 1024))
+# X_test = np.arange(len(X_test) * 1024).reshape((len(X_test), 1024))
+
+# assert len(X_train) == len(train_labels), 'features must be same size as labels'
+
+X_train = flatten_images(X_train)
+X_test = flatten_images(X_test)
+print (X_train.shape)
+
+X_train = normalize_greyscale(X_train)
+X_test = normalize_greyscale(X_test)
+print (X_train.shape)
 num_channels = 1
 
 
@@ -121,12 +160,12 @@ def compute_dimensions(train_features, test_features):
 
     return n_train, n_test, num_channels, image_shape, labels_count, image_size, img_size_flat
 
-n_train, n_test, num_channels, image_shape, labels_count, image_size, img_size_flat = compute_dimensions(train_features, test_features)
+n_train, n_test, num_channels, image_shape, labels_count, image_size, img_size_flat = compute_dimensions(X_train, X_test)
 
 
 def next_batch(batch_size, fake_data=False):
     """Return the next `batch_size` examples from this data set."""
-    global _index_in_epoch, _num_examples, _epochs_completed, train_features, train_labels
+    global _index_in_epoch, _num_examples, _epochs_completed, X_train, train_labels
 
     start = _index_in_epoch
     _index_in_epoch += batch_size
@@ -136,14 +175,14 @@ def next_batch(batch_size, fake_data=False):
         # Shuffle the data
         perm = np.arange(_num_examples)
         np.random.shuffle(perm)
-        train_features = train_features[perm]
+        X_train = X_train[perm]
         train_labels = train_labels[perm]
         # Start next epoch
         start = 0
         _index_in_epoch = batch_size
         assert batch_size <= _num_examples
     end = _index_in_epoch
-    return train_features[start:end], train_labels[start:end]
+    return X_train[start:end], train_labels[start:end]
 
 
 
@@ -163,40 +202,66 @@ print('Labels One-Hot Encoded')
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Get randomized datasets for training and validation
-print('train_features before split: ', len(train_features))
+print('train_features before split: ', len(X_train))
 print('train_labels before split: ', len(train_labels))
-print('test_features before split: ', len(test_features))
+print('test_features before split: ', len(X_test))
 print('test_labels before split: ', len(test_labels))
-train_features, valid_features, train_labels, valid_labels = train_test_split(
-    train_features,
+X_train, valid_features, train_labels, valid_labels = train_test_split(
+    X_train,
     train_labels,
-    test_size=0.05,
+    test_size=0.15,
     random_state=832289)
 
 print('Training features and labels randomized and split.')
 
-print('train_features after split: ', len(train_features))
+print('train_features after split: ', len(X_train))
 print('train_labels after split: ', len(train_labels))
-print('test_features after split: ', len(test_features))
+print('test_features after split: ', len(X_test))
 print('test_labels after split: ', len(test_labels))
+
+_epochs_completed = 0
+_index_in_epoch = 0
+_num_examples = len(X_train)
 
 
 features_count = image_size*num_channels  # TRAFFIC SIGNS data input (img shape: 32*32)
 
-a_mode = 2
+a_mode = 1
 b_mode = 1
 
 if a_mode == 1:
     # Parameters
     learning_rate = 0.2
-    training_epochs = 5
-    batch_size = 100
+    training_epochs = 50
+    batch_size = 50
     display_step = 1
 
-    # labels_count = labels_count  # TRAFFIC SIGNS total classes (0-43 digits)
-
-    n_hidden_layer = 512 # layer number of features
+    n_hidden_layer = 256 # layer number of features
 
     # Store layers weight & bias
     weights = {
@@ -209,28 +274,25 @@ if a_mode == 1:
     }
 
     # tf Graph input
-    x = tf.placeholder("float", [None, image_shape[0], image_shape[1], num_channels])
+    x = tf.placeholder("float", [None, image_size])
     y = tf.placeholder("float", [None, labels_count])
 
-    # x_flat = tf.reshape(x, [-1, features_count])
-    x_flat = tf.reshape(x, [-1,image_shape[0], image_shape[1], num_channels])
+    x_flat = tf.reshape(x, [-1, features_count])
+    # x_flat = tf.reshape(x, [-1, image_shape[0], image_shape[1], num_channels])
 
     # Hidden layer with RELU activation
     layer_1 = tf.add(tf.matmul(x_flat, weights['hidden_layer']), biases['hidden_layer'])
     layer_1 = tf.nn.relu(layer_1)
+
     # Output layer with linear activation
     logits = tf.matmul(layer_1, weights['out']) + biases['out']
 
     if b_mode == 1:
-        # Dropout prevention
-        keep_prob = tf.placeholder(tf.float32)
-        h_fc1_drop = tf.nn.dropout(layer_1, keep_prob)
-
         # Define loss and optimizer
-        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, y)) # also called cross_entropy
+        # cost also called cross_entropy
+        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, y))
         optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
 
-        # Initializing the variables
         init = tf.initialize_all_variables()
 
         # Launch the graph
@@ -247,18 +309,25 @@ if a_mode == 1:
                 # Display logs per epoch step
                 if epoch % display_step == 0:
                     c = sess.run(cost, feed_dict={x: batch_x, y: batch_y})
-                    print("Epoch:", '%04d' % (epoch+1), "cost=", \
-                        "{:.9f}".format(c))
+                    print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c))
+                    # Calculate accuracy
+                    correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(y, 1))
+                    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+                    print("Accuracy:", accuracy.eval({x: X_test, y: test_labels}))
             print("Optimization Finished!")
 
             # Test model
-            # train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-            correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(y, 1))
+            # train_step = tf.train.AdamOptimizer(1e-4).minimize(cost)
             # Calculate accuracy
+            correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(y, 1))
             accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-            print("Accuracy:", accuracy.eval({x: test_features, y: test_labels}))
+            print("Accuracy:", accuracy.eval({x: X_test, y: test_labels}))
 
     elif b_mode == 2:
+        # Dropout prevention
+        keep_prob = tf.placeholder(tf.float32)
+        h_fc1_drop = tf.nn.dropout(layer_1, keep_prob)
+
         # Launch the graph
         with tf.Session() as sess:
             cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, y))
@@ -273,7 +342,7 @@ if a_mode == 1:
                     print("step %d, training accuracy %g" % (i, train_accuracy))
                 train_step.run(feed_dict={x: batch[0], y: batch[1], keep_prob: 0.5})
 
-            print("test accuracy %g" % accuracy.eval(feed_dict={x: mnist.test.images, y: mnist.test.labels, keep_prob: 1.0}))
+            print("test accuracy %g" % accuracy.eval(feed_dict={x: X_test, y: test_labels, keep_prob: 1.0}))
 
 elif a_mode == 2:
     print("DO lesson_7_miniflow lab process")
@@ -304,17 +373,24 @@ elif a_mode == 2:
     assert labels._dtype == tf.float32, 'labels must be type float32'
 
     # Feed dicts for training, validation, and test session
-    train_feed_dict = {features: train_features, labels: train_labels}
+    train_feed_dict = {features: X_train, labels: train_labels}
     valid_feed_dict = {features: valid_features, labels: valid_labels}
-    test_feed_dict = {features: test_features, labels: test_labels}
+    test_feed_dict = {features: X_test, labels: test_labels}
 
     # Linear Function WX + b
     logits = tf.matmul(features, weights) + biases
 
+    # From Sridhar Sampath in forums at https://carnd-udacity.atlassian.net/wiki/questions/12617346/answers/12620228
+    logits = -np.amax(logits)
+
     prediction = tf.nn.softmax(logits)
 
     # Cross entropy
-    cross_entropy = -tf.reduce_sum(labels * tf.log(prediction), reduction_indices=1)
+    # cross_entropy = -tf.reduce_sum(labels * tf.log(prediction), reduction_indices=1)
+    # From Vivek forum tip at:
+    #
+    # https://carnd-udacity.atlassian.net/wiki/cq/viewquestion.action?id=12617346&questionTitle=what-could-be-causing-very-low-accuracy
+    cross_entropy = tf.reduce_mean(-tf.reduce_sum(labels * tf.log(tf.clip_by_value(prediction, 1e-10, 1.0)), reduction_indices=[1]))
 
     # Training loss
     loss = tf.reduce_mean(cross_entropy)
@@ -336,6 +412,7 @@ elif a_mode == 2:
 
     # Determine if the predictions are correct
     is_correct_prediction = tf.equal(tf.argmax(prediction, 1), tf.argmax(labels, 1))
+
     # Calculate the accuracy of the predictions
     accuracy = tf.reduce_mean(tf.cast(is_correct_prediction, tf.float32))
 
@@ -344,9 +421,9 @@ elif a_mode == 2:
 
     # ToDo: Find the best parameters for each configuration
     # Validation accuracy at 0.8085333108901978
-    epochs = 5
+    epochs = 15
     batch_size = 50
-    learning_rate = 0.2
+    learning_rate = 0.0001
 
     ### DON'T MODIFY ANYTHING BELOW ###
     # Gradient Descent
@@ -364,7 +441,7 @@ elif a_mode == 2:
 
     with tf.Session() as session:
         session.run(init)
-        batch_count = int(math.ceil(len(train_features)/batch_size))
+        batch_count = int(math.ceil(len(X_train) / batch_size))
 
         for epoch_i in range(epochs):
 
@@ -375,7 +452,7 @@ elif a_mode == 2:
             for batch_i in batches_pbar:
                 # Get a batch of training features and labels
                 batch_start = batch_i*batch_size
-                batch_features = train_features[batch_start:batch_start + batch_size]
+                batch_features = X_train[batch_start:batch_start + batch_size]
                 batch_labels = train_labels[batch_start:batch_start + batch_size]
 
                 # Run optimizer and get loss
@@ -396,8 +473,10 @@ elif a_mode == 2:
                     train_acc_batch.append(training_accuracy)
                     valid_acc_batch.append(validation_accuracy)
 
+
             # Check accuracy against Validation data
             validation_accuracy = session.run(accuracy, feed_dict=valid_feed_dict)
+            print('Epoch {}, validation accuracy {}'.format(epoch_i, validation_accuracy))
 
     loss_plot = plt.subplot(211)
     loss_plot.set_title('Loss')
@@ -426,9 +505,9 @@ elif a_mode == 2:
     """
 
     # ToDo: Set the epochs, batch_size, and learning_rate with the best parameters from problem 3
-    epochs = 5
+    epochs = 15
     batch_size = 50
-    learning_rate = 0.1
+    learning_rate = 0.0001
 
     ### DON'T MODIFY ANYTHING BELOW ###
     # The accuracy measured against the test set
@@ -436,7 +515,7 @@ elif a_mode == 2:
 
     with tf.Session() as session:
         session.run(init)
-        batch_count = int(math.ceil(len(train_features) / batch_size))
+        batch_count = int(math.ceil(len(X_train) / batch_size))
 
         for epoch_i in range(epochs):
 
@@ -447,7 +526,7 @@ elif a_mode == 2:
             for batch_i in batches_pbar:
                 # Get a batch of training features and labels
                 batch_start = batch_i * batch_size
-                batch_features = train_features[batch_start:batch_start + batch_size]
+                batch_features = X_train[batch_start:batch_start + batch_size]
                 batch_labels = train_labels[batch_start:batch_start + batch_size]
 
                 # Run optimizer
