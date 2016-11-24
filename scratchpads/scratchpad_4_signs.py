2a98872c28fcf551b3cf11b0904ed0fc85efaf68
Matthew Zimmer <M16r01@gmail.com>
2016-11-24 15:49:46 -0500

    renamed pipeline to datasets.

--- scratchpads/scratchpad_4_signs.py
+++ scratchpads/scratchpad_4_signs.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 from sklearn.preprocessing import LabelBinarizer
 from tqdm import tqdm
 
-from pipelines.german_traffic_sign_dataset import GermanTrafficSignDataset
+from datasets.german_traffic_sign_dataset import GermanTrafficSignDataset
 
 """
 Helper-function for flattening a layer
